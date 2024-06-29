34e599ddb9f0603b501346152165c27e91da8ebb
scaramallion <scaramallion@users.noreply.github.com>
2024-06-29 11:12:01 +1000

    Automated package update (#6)
    Co-authored-by: scaramallion <10226372+scaramallion@users.noreply.github.com>

--- src/pydicom-stubs/sr/__init__.pyi
+++ src/pydicom-stubs/sr/__init__.pyi
@@ -1,1 +1,2 @@
-from pydicom.sr.codedict import codes as codes
+from pydicom.sr.codedict import Collection as Collection, Concepts as Concepts, codes as codes
+from pydicom.sr.coding import Code as Code
